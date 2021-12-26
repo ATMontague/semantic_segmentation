ccfa40ae0704073f0ce96a7738913ae4c4e52dd5
adam <atmontague10@gmail.com>
2021-12-25 17:06:56 -0800

    making bounding boxes

--- util/contours.py
+++ util/contours.py
@@ -2,13 +2,6 @@ import cv2
 import imutils
 import argparse
 
-'''
-using contour detection we can find the center of specified regions
-
-goal: find center of each polyp prediction map
-2ndary goal: find the number of polyps present in image
-'''
-
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -24,12 +17,6 @@ def main():
 
     COLOR = (0, 0, 255)
 
-    # img = '../data/Kvasir-SEG/images/cju0qx73cjw570799j4n5cjze.jpg'
-    # img = cv2.imread(img)
-    # mask = '../data/Kvasir-SEG/masks/cju0qx73cjw570799j4n5cjze.jpg'
-    # mask_rgb = cv2.imread(mask)
-    # mask = cv2.imread(mask, cv2.IMREAD_GRAYSCALE)
-
     img = cv2.imread(args.image)
     mask_rgb = cv2.imread(args.mask)
     mask = cv2.imread(args.mask, cv2.IMREAD_GRAYSCALE)
@@ -38,15 +25,10 @@ def main():
 
     # identify contours
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    # draw contour lines and centroid for each polyp detected
+    # draw bounding box
     for c in contours:
-        cv2.drawContours(mask_rgb, c, -1, COLOR, 3)
-
-        M = cv2.moments(c)
-        c_x = int(M['m10'] / M['m00'])
-        c_y = int(M['m01'] / M['m00'])
-        cv2.circle(img=mask_rgb, center=(c_x, c_y), radius=10, color=(0, 255, 0), thickness=-1)
-
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(mask_rgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imwrite('output.png', mask_rgb)
 
